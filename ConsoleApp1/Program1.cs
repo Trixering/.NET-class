0009b8ef1d8399358afd44f9407287c00087899a
User <User@LAPTOP-BI9DTH0B>
2025-09-23 14:51:48 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        string[] data = { "\u5B78\u865F：11244151", "\u59D3\u540D：\u5289\u9053\u5143" };
+
+        foreach (string item in data)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
